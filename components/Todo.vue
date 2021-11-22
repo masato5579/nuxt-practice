13251366b7e2fb21daa8b424d81d5e51ad5a5a08
masato5579 <73378197+masato5579@users.noreply.github.com>
2021-11-23 00:19:46 +0900

    [add]環境構築＆雛形作成

---
+++ components/Todo.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="container">
+    <div class="taskAdd">
+      <input
+        type="text"
+        name="input"
+        id="input"
+        placeholder="タスクを入力してください"
+      />
+      <button id="add" class="button">追加</button>
+    </div>
+    <div class="filter">
+      <button class="button">全て</button>
+      <button class="button">完了</button>
+      <button class="button">未完了</button>
+    </div>
+    <div class="task">
+      <p class="taskContent">
+        テストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテスト
+        テストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテスト
+      </p>
+      <button>完了</button>
+    </div>
+  </section>
+</template>
+<style>
+</style>
