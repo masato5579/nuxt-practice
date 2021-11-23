c5ea4ff8e8a6fd10d79f81b2439ef59434e5d01e
masato5579 <73378197+masato5579@users.noreply.github.com>
2021-11-23 22:11:25 +0900

    [fix]style.cssを全体に読み込むように

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '~assets/css/reset.css'
+    '~assets/css/reset.css',
+    '~assets/css/style.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -50,4 +51,3 @@ export default {
   build: {
   }
 }
-      
